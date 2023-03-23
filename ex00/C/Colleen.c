56330f20d244ae3ab0957ccb661ce760115f729c
takashi247 <takashi247@gmail.com>
2023-03-24 01:04:04 +0900

    add: additional function in Colleen in C

--- ex00/C/Colleen.c
+++ ex00/C/Colleen.c
@@ -3,18 +3,29 @@
 */
 
 #include <stdio.h>
-const char *s = "/*$    Colleen: This program will print its own source in stdout when run.$*/$$#include <stdio.h>$const char *s = @;$int main() {$    for (const char *p = s; *p; ++p) {$        if (*p == 64) {$            putchar(34);$            fputs(s, stdout);$            putchar(34);$        } else if (*p == 36) {$            putchar(10);$        } else {$            putchar(*p);$        }$    }$    return 0;$}$";
-int main() {
+const char *s = "/*$    Colleen: This program will print its own source in stdout when run.$*/$$#include <stdio.h>$const char *s = @;$$void    ft_put_quine_char(const char c)${$    if (c == 64) {$        putchar(34);$        fputs(s, stdout);$        putchar(34);$    } else if (c == 36) {$        putchar(10);$    } else {$        putchar(c);$    }$}$$int     main(void)${$    for (const char *p = s; *p; ++p) {$        /*$            ft_put_quine_char: put NL when dollar sign, put *s when at mark,$            otherwise put c$        */$        ft_put_quine_char(*p);$    }$    return 0;$}$";
+
+void    ft_put_quine_char(const char c)
+{
+    if (c == 64) {
+        putchar(34);
+        fputs(s, stdout);
+        putchar(34);
+    } else if (c == 36) {
+        putchar(10);
+    } else {
+        putchar(c);
+    }
+}
+
+int     main(void)
+{
     for (const char *p = s; *p; ++p) {
-        if (*p == 64) {
-            putchar(34);
-            fputs(s, stdout);
-            putchar(34);
-        } else if (*p == 36) {
-            putchar(10);
-        } else {
-            putchar(*p);
-        }
+        /*
+            ft_put_quine_char: put NL when dollar sign, put *s when at mark,
+            otherwise put c
+        */
+        ft_put_quine_char(*p);
     }
     return 0;
 }
