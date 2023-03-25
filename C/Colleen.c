3ee6c0b9d77255f23698c33cc673ef027e3d6c65
takashi247 <takashi247@gmail.com>
2023-03-25 21:25:30 +0900

    add: macro variables to improve readability

--- C/Colleen.c
+++ C/Colleen.c
@@ -3,16 +3,22 @@
 */
 
 #include <stdio.h>
-const char *s = "/*$    Colleen: This program will print its own source in stdout when run.$*/$$#include <stdio.h>$const char *s = @;$$void    ft_put_quine_char(const char c)${$    if (c == 64) {$        putchar(34);$        fputs(s, stdout);$        putchar(34);$    } else if (c == 36) {$        putchar(10);$    } else {$        putchar(c);$    }$}$$int     main(void)${$    for (const char *p = s; *p; ++p) {$        /*$            ft_put_quine_char: put NL when dollar sign, put *s when at mark,$            otherwise put c$        */$        ft_put_quine_char(*p);$    }$    return 0;$}$";
+
+#define ATMARK 64
+#define DQUOTE 34
+#define DOLLAR 36
+#define NEWLINE 10
+
+const char *s = "/*$    Colleen: This program will print its own source in stdout when run.$*/$$#include <stdio.h>$$#define ATMARK 64$#define DQUOTE 34$#define DOLLAR 36$#define NEWLINE 10$$const char *s = @;$$void    ft_put_quine_char(const char c)${$    if (c == ATMARK) {$        putchar(DQUOTE);$        fputs(s, stdout);$        putchar(DQUOTE);$    } else if (c == DOLLAR) {$        putchar(NEWLINE);$    } else {$        putchar(c);$    }$}$$int     main(void)${$    for (const char *p = s; *p; ++p) {$        /*$            put NL when dollar sign, put *s surrounded by d-quote when at mark,$            otherwise put c$        */$        ft_put_quine_char(*p);$    }$    return 0;$}$";
 
 void    ft_put_quine_char(const char c)
 {
-    if (c == 64) {
-        putchar(34);
+    if (c == ATMARK) {
+        putchar(DQUOTE);
         fputs(s, stdout);
-        putchar(34);
-    } else if (c == 36) {
-        putchar(10);
+        putchar(DQUOTE);
+    } else if (c == DOLLAR) {
+        putchar(NEWLINE);
     } else {
         putchar(c);
     }
@@ -22,7 +28,7 @@ int     main(void)
 {
     for (const char *p = s; *p; ++p) {
         /*
-            ft_put_quine_char: put NL when dollar sign, put *s when at mark,
+            put NL when dollar sign, put *s surrounded by d-quote when at mark,
             otherwise put c
         */
         ft_put_quine_char(*p);
